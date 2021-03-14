a36b2a78675aef7c56d0e349ea652bbcff1ae2e8
keremcanb <keremcan@gmail.com>
2021-03-14 23:12:08 +0300

    error page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Dashboard />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Dashboard />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="*">
+        <Error />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
