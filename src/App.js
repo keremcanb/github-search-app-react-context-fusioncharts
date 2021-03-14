1bf3e038bdefb65423ad4be90a06e86a3f64cccc
keremcanb <keremcan@gmail.com>
2021-03-14 23:02:18 +0300

    router setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Dashboard />
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
