4682c61be303a4961028fddcfdc3df23fc216dbe
keremcanb <keremcan@gmail.com>
2021-03-18 23:02:36 +0300

    search functionality

--- src/context/context.js
+++ src/context/context.js
@@ -9,16 +9,63 @@ const rootUrl = 'https://api.github.com';
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
-  const [githubUser, setGithubUser] = useState(mockUser);
-  const [repos, setRepos] = useState(mockRepos);
-  const [followers, setFollowers] = useState(mockFollowers);
+  const [githubUser, setGithubUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [followers, setFollowers] = useState([]);
+
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const res = await axios(`${rootUrl}/users/${user}`).catch((err) => console.log(err));
+
+    if (res) {
+      setGithubUser(res.data);
+      const { login, followers_url } = res.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`)
+      ])
+        .then((results) => {
+          // eslint-disable-next-line no-shadow
+          const [repos, followers] = results;
+          const status = 'fulfilled';
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      toggleError(true, 'there is no user with that username');
+    }
+    setIsLoading(false);
+  };
+
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+
+  useEffect(() => {
+    searchGithubUser('keremcanb');
+  }, []);
 
   return (
     <GithubContext.Provider
       value={{
         githubUser,
         repos,
-        followers
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading
       }}
     >
       {children}
