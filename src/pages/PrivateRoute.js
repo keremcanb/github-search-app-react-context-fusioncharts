2f9d4c563c4b96b6a5c85f9c7267ca0b570b00f9
keremcanb <keremcan@gmal.com>
2021-03-25 00:22:03 +0300

    comments

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
+// Children is dashboard, grap rest of the props
 const PrivateRoute = ({ children, ...rest }) => {
   const { isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
 
   return (
     <Route
+      // Spread all props from route
       {...rest}
       render={() => {
         return isUser ? children : <Redirect to="/login" />;
